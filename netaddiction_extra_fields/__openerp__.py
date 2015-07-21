4fb34244f89426fedb839ff2be53534490f742a0
Matteo Piciucchi <matteo@localmatteo.com>
2015-07-21 17:33:26 +0200

    Aggiunta prima parte modulo EXTRA FIELDS

---
+++ netaddiction_extra_fields/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Netaddiction Extra Fields",
+    'summary': "Aggiunge campi extra ad alcuni moduli",
+    'description':"""Aggiunge campi extra ad alcuni moduli per il corretto
+        funzionamento degli shop\n
+
+        PRODUCT.PRODUCT:\n
+
+        1 - Override dei campi 'type' = 'ogni variante può essere un tipo differente'
+        2 - Aggiunta campi 'published' = 'visibile o no sul sito',
+            'out_date' = 'Data di uscita',
+            'out_date_approx_type' = 'tipo di approssimazione sulla data'
+        """,
+    'author': "Netaddiction",
+    'website': "http://www.netaddiction.it",
+    'category': 'Technical Settings',
+    'version': '0.1',
+    'depends': ['base','product','sale','purchase'],
+    'data' :[
+
+    ]
+}
---
+++ netaddiction_extra_fields/models/products.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class Products(models.Model):
+    _inherit = 'product.product'
+
+    #campi override per rendere le varianti indipendenti dai template
+    type = fields.Selection((('consu', 'Consumable'),('service','Service'),
+        ('product','Stockable Product')), string='Product Type')
+
+    #campi aggiunti
+    published = fields.Boolean(string="Visibile sul Sito?")
+    out_date = fields.Date(string="Data di Uscita")
+    out_date_approx_type = fields.Selection(string="Approssimazione Data",
+        selection=(('accurate','Preciso'),('month','Mensile'),('quarter','Trimestrale'),
+        ('four','Quadrimestrale'),('year','Annuale')))
