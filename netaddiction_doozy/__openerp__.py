8123c8c4edaf973a5cec4bda3f505e8a832f3d44
Yuri Farina <yuri.farina@multiplayer.it>
2015-08-28 12:13:10 +0200

    Aggiunto modulo NetAddiction Doozy

---
+++ netaddiction_doozy/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "NetAddiction Doozy",
+    'summary': "Piccole migliorie all'interfaccia",
+    'description': """
+        * Toglie il reference ID dal nome del prodotto
+        * Aggiunge il menu Bundle all'area Vendite
+        * Nasconde il campo per l'EAN13
+    """,
+    'author': "NetAddiction",
+    'website': "http://www.netaddiction.it",
+    'category': 'Technical Settings',
+    'depends': ['base', 'product'],
+    'version': '0.1',
+    'data': [
+        'views/product_view.xml',
+    ]
+}
