7a38f4af9b63c70a7c34a2484411f02947e70f27
Matteo Piciucchi <matteo@localmatteo.com>
2015-09-02 16:59:11 +0200

    traduzione in doozy, campo final price etc

--- netaddiction_extra_fields/models/products.py
+++ netaddiction_extra_fields/models/products.py
@@ -8,7 +8,7 @@ class Products(models.Model):
     #campi override per rendere le varianti indipendenti dai template
     type = fields.Selection((('consu', 'Consumable'),('service','Service'),
         ('product','Prodotto Stoccabile')), string='Tipo Prodotto', _translate="True")
-    lst_price = fields.Float(string="Prezzo")
+    lst_price = fields.Float(string="Prezzo senza Iva")
     list_price = fields.Float(string="Prezzo Listino")
 
     #campi aggiunti
@@ -18,6 +18,50 @@ class Products(models.Model):
         selection=(('accurate','Preciso'),('month','Mensile'),('quarter','Trimestrale'),
         ('four','Quadrimestrale'),('year','Annuale')))
 
+    #campo prezzo ivato
+    final_price = fields.Float(string="Prezzo al pubblico")
+
+    @api.multi
+    def write(self,values):
+        tassa = self.taxes_id.amount
+        final = self.final_price
+
+        if 'taxes_id' in values.keys():
+            id_tax = values['taxes_id'][0][2]
+            if len(id_tax)>0:
+                id_tax = id_tax[0]
+
+            tassa = self.env['account.tax'].search([('id','=',id_tax)]).amount
+
+        if 'final_price' in values.keys():
+            final = values['final_price']
+
+        detax = final / (float(1) + tassa)
+        deiva = round(detax,2)
+        values['list_price'] = deiva
+
+        return super(Products, self).write(values)
+
+        @api.multi
+        def create(self,values):
+            tassa = self.taxes_id.amount
+            final = self.final_price
+
+            if 'taxes_id' in values.keys():
+                id_tax = values['taxes_id'][0][2]
+                if len(id_tax)>0:
+                    id_tax = id_tax[0]
+
+                tassa = self.env['account.tax'].search([('id','=',id_tax)]).amount
+
+            if 'final_price' in values.keys():
+                final = values['final_price']
+
+            detax = final / (float(1) + tassa)
+            deiva = round(detax,2)
+            values['list_price'] = deiva
+
+            return super(Products, self).create(values)
 
 class Template(models.Model):
     _inherit = 'product.template'
@@ -25,6 +69,8 @@ class Template(models.Model):
     #campi override per rendere le varianti indipendenti dai template
     type = fields.Selection((('consu', 'Consumable'),('service','Service'),
         ('product','Prodotto Stoccabile')), string='Tipo Prodotto', _translate="True")
+    lst_price = fields.Float(string="Prezzo senza Iva")
+    list_price = fields.Float(string="Prezzo Listino")
 
     #campi aggiunti
     published = fields.Boolean(string="Visibile sul Sito?")
@@ -32,3 +78,5 @@ class Template(models.Model):
     out_date_approx_type = fields.Selection(string="Approssimazione Data",
         selection=(('accurate','Preciso'),('month','Mensile'),('quarter','Trimestrale'),
         ('four','Quadrimestrale'),('year','Annuale')))
+    #campo prezzo ivato
+    final_price = fields.Float(string="Prezzo al pubblico")
