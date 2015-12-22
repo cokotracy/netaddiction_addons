ba4bb382cc39ef075cfa4677334a884cc2a6e1bf
andrea.bozzi <andreabozzi@MacBook-di-andreabozzi.local>
2015-12-22 17:59:27 +0100

    controlli sui campi

--- netaddiction_extra_fields/models/gift.py
+++ netaddiction_extra_fields/models/gift.py
@@ -1,24 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from openerp.exceptions import ValidationError
 
 class Gift(models.Model):
     _name = "netaddiction.gift"
     partner_id = fields.Many2one(
         comodel_name='res.partner',
-        string="Cliente")
+        string="Cliente", required=True)
     type_id = fields.Many2one(
         comodel_name='netaddiction.gift.type',
-        string="Type")
+        string="Type", required = True)
     value = fields.Float(string="Valore")
-    reason = fields.Selection([('r','Rimborso'),('a','Altro')], string='Motivazione', default='r')
+    #reason = fields.Selection([('r','Rimborso'),('a','Altro')], string='Motivazione', default='r')
 
     @api.one
     @api.constrains('value')
     def _check_value(self):
+        print "VALUE"
+        print self.value
+        print self.type_id.reason
         if self.value < 0.0:
             self.value = 0.0
             raise ValidationError("Il valore del gift deve essere maggiore uguale a zero")
+
+    @api.one
+    @api.constrains('partner_id')
+    def _check_partner_id(self):
+
+        if self.partner_id is None or self.partner_id.type != 'contact' :
+            raise ValidationError("Il gift può essere assegnato solo a clienti")
+
+   
 
 class GiftCustomer(models.Model):
     _inherit = 'res.partner'
@@ -38,6 +51,7 @@ class GiftCustomer(models.Model):
 
 class GiftType(models.Model):
     _name = "netaddiction.gift.type"
+    _rec_name = 'reason'
     reason = fields.Char('Motivazione')
     priority = fields.Selection([(0,'Molto Alta'),(1,'Alta'),(2,'Media'),(3,'Bassa'),(4,'Molto Bassa')], string='Priorità', default=2)
     gift_ids = fields.One2many(
