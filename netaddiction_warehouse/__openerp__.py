7b3b204b5fde5d6aa902bb5b41e74ba567c49b37
Matteo Piciucchi <matteo@localmatteo.com>
2016-01-11 10:35:25 +0100

    [ADD] magazzino, eliminato il vecchio wh locations

---
+++ netaddiction_warehouse/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Netaddiction Warehouse",
+    'summary': "Magazzino Netaddiction",
+    'description':"""
+     """,
+    'author': "Netaddiction",
+    'website': "http://www.netaddiction.it",
+    'category': 'Technical Settings',
+    'version': '1.0',
+    'depends': ['base','product','sale','purchase','mrp','stock'],
+    'data' :[
+        'views/locations.xml',
+    ]
+}
