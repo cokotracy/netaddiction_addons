bf1e2e13a0351e6ace21d9affcaf5abda9ae4018
andrea.bozzi <andreabozzi@macbookreabozzi.multiplayer.int>
2016-01-22 18:06:21 +0100

    finiti stati ordine e fattura

--- netaddiction_orders/models/orders.py
+++ netaddiction_orders/models/orders.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from openerp import tools
 from openerp import models, fields, api
+from openerp.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
+
 
 class Orders(models.Model):
     _inherit = 'sale.order'
@@ -13,32 +15,29 @@ class Orders(models.Model):
         ('problem', 'Problema'),
         ('done', 'Completato'),
         ('cancel', 'Annullato'),
-        ], string='Status', readonly=True, copy=False, index=True)
+    ], string='Status', readonly=True, copy=False, index=True)
 
     ip_address = fields.Char(string="Indirizzo IP")
-    delivery_option = fields.Selection([('all','tutto insieme'),('asap','non appena disponibile')], string='Opzione spedizione')
-
-    order_line = fields.One2many('sale.order.line', 'order_id', string='Order Lines', states={'cancel': [('readonly', True)], 'done': [('readonly', True)], 'sale': [('readonly', True)], 'partial_done': [('readonly', True)]}, copy=True)
-
-
+    delivery_option = fields.Selection([('all', 'tutto insieme'), ('asap', 'non appena disponibile')],
+                                       string='Opzione spedizione')
 
     ##############
-    #ACTION STATE#
+    # ACTION STATE#
     ##############
 
-    @api.one 
+    @api.one
     def action_problems(self):
         self.state = 'problem'
 
-    @api.one 
+    @api.one
     def action_partial_done(self):
         self.state = 'partial_done'
 
     ##########
-    #OVERRIDE#
+    # OVERRIDE#
     ##########
 
-    #Toglie il controllo sullo stato 'draft' per l'aggiunta delle spese di spedizione
+    # Toglie il controllo sullo stato 'draft' per l'aggiunta delle spese di spedizione
     @api.multi
     def delivery_set(self):
         # Remove delivery products from the sale order
@@ -47,7 +46,7 @@ class Orders(models.Model):
         for order in self:
             carrier = order.carrier_id
             if carrier:
-                #if order.state not in ('draft', 'sent'):
+                # if order.state not in ('draft', 'sent'):
                 #    raise UserError(_('The order state have to be draft to add delivery lines.'))
 
                 if carrier.delivery_type not in ['fixed', 'base_on_rule']:
@@ -60,12 +59,56 @@ class Orders(models.Model):
                         raise UserError(_('No carrier matching.'))
                     price_unit = carrier.get_price_available(order)
                     if order.company_id.currency_id.id != order.pricelist_id.currency_id.id:
-                        price_unit = order.company_id.currency_id.with_context(date=order.date_order).compute(price_unit, order.pricelist_id.currency_id)
+                        price_unit = order.company_id.currency_id.with_context(date=order.date_order).compute(
+                            price_unit, order.pricelist_id.currency_id)
 
                 order._create_delivery_line(carrier, price_unit)
 
             else:
                 raise UserError(_('No carrier set for this order.'))
+
+
+    @api.depends('state', 'order_line.invoice_status')
+    def _get_invoiced(self):
+        """
+        Compute the invoice status of a SO. Possible statuses:
+        - no: if the SO is not in status 'sale' or 'done', we consider that there is nothing to
+          invoice. This is also hte default value if the conditions of no other status is met.
+        - to invoice: if any SO line is 'to invoice', the whole SO is 'to invoice'
+        - invoiced: if all SO lines are invoiced, the SO is invoiced.
+        - upselling: if all SO lines are invoiced or upselling, the status is upselling.
+
+        The invoice_ids are obtained thanks to the invoice lines of the SO lines, and we also search
+        for possible refunds created directly from existing invoices. This is necessary since such a
+        refund is not directly linked to the SO.
+        """
+        print "ORDER HERE"
+        for order in self:
+            invoice_ids = order.order_line.mapped('invoice_lines').mapped('invoice_id')
+            # Search for refunds as well
+            refund_ids = self.env['account.invoice'].browse()
+            if invoice_ids:
+                refund_ids = refund_ids.search([('type', '=', 'out_refund'), ('origin', 'in', invoice_ids.mapped('number')), ('origin', '!=', False)])
+
+            line_invoice_status = [line.invoice_status for line in order.order_line]
+
+            if order.state not in ('sale', 'done','partial_done'):
+                invoice_status = 'no'
+            elif any(invoice_status == 'to invoice' for invoice_status in line_invoice_status):
+                invoice_status = 'to invoice'
+            elif all(invoice_status == 'invoiced' for invoice_status in line_invoice_status):
+                invoice_status = 'invoiced'
+            elif all(invoice_status in ['invoiced', 'upselling'] for invoice_status in line_invoice_status):
+                invoice_status = 'upselling'
+            else:
+                invoice_status = 'no'
+            print invoice_status
+
+            order.update({
+                'invoice_count': len(set(invoice_ids.ids + refund_ids.ids)),
+                'invoice_ids': invoice_ids.ids + refund_ids.ids,
+                'invoice_status': invoice_status
+            })
 
     @api.multi
     def action_cancel(self):
@@ -83,15 +126,116 @@ class Orders(models.Model):
         # TODO: modificare mittente e testo mail
         body_html = '''cancellato ordine'''
         values = {
-            'subject' : 'ordine cancellato',
+            'subject': 'ordine cancellato',
             'body_html': body_html,
-            'email_from' : 'no-reply',
-            'email_to' : self.partner_id.email,
+            'email_from': 'no-reply',
+            'email_to': self.partner_id.email,
         }
 
         email = self.env['mail.mail'].create(values)
         email.send()
 
 
-        
+
+    @api.multi
+    def _check_action_done(self):
+        self.ensure_one()
+        if all(line.qty_invoiced == line.qty_delivered == line.product_uom_qty for line in self.order_line):
+            if (self.state == 'sale' or self.state == 'partial_done'):
+                self.action_done()
+
+    @api.multi
+    def _check_partially_done(self):
+        self.ensure_one()
+        if (self.state == 'sale'):
+                self.write({'state': 'partial_done'})
+
+
+
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    state = fields.Selection([
+        ('draft', 'Quotation'),
+        ('sent', 'Quotation Sent'),
+        ('sale', 'Sale Order'),
+        ('partial_done', 'Parzialmente Completato'),
+        ('problem', 'Problema'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled'),
+    ], related='order_id.state', string='Order Status', readonly=True, copy=False, store=True, default='draft')
+
+    invoice_status = fields.Selection([
+        ('upselling', 'Upselling Opportunity'),
+        ('invoiced', 'Fully Invoiced'),
+        ('to invoice', 'To Invoice'),
+        ('no', 'Nothing to Invoice')
+        ], string='Invoice Status', compute='_compute_invoice_status', store=True, readonly=True, default='no')
+
+
+    @api.constrains('qty_delivered','qty_invoiced')
+    def _check_complete(self):
+        for line in self:
+            if (line.qty_invoiced == line.qty_delivered == line.product_uom_qty):
+                line.order_id._check_action_done()
+            elif (line.qty_delivered > 0):
+                line.order_id._check_partially_done()
+ 
+    ##########
+    # OVERRIDE#
+    ##########
+
+    # Aggiungo partial done agli stati in cui si può fatturare
+
+    @api.depends('state', 'product_uom_qty', 'qty_delivered', 'qty_to_invoice', 'qty_invoiced')
+    def _compute_invoice_status(self):
+        """
+        Compute the invoice status of a SO line. Possible statuses:
+        - no: if the SO is not in status 'sale' or 'done', we consider that there is nothing to
+          invoice. This is also hte default value if the conditions of no other status is met.
+        - to invoice: we refer to the quantity to invoice of the line. Refer to method
+          `_get_to_invoice_qty()` for more information on how this quantity is calculated.
+        - upselling: this is possible only for a product invoiced on ordered quantities for which
+          we delivered more than expected. The could arise if, for example, a project took more
+          time than expected but we decided not to invoice the extra cost to the client. This
+          occurs onyl in state 'sale', so that when a SO is set to done, the upselling opportunity
+          is removed from the list.
+        - invoiced: the quantity invoiced is larger or equal to the quantity ordered.
+        """
+        print "ORDER LINE HERE"
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        for line in self:
+            if line.state not in ('sale', 'done','partial_done'):
+                line.invoice_status = 'no'
+            elif not float_is_zero(line.qty_to_invoice, precision_digits=precision):
+                line.invoice_status = 'to invoice'
+            elif line.state == 'sale' and line.product_id.invoice_policy == 'order' and\
+                    float_compare(line.qty_delivered, line.product_uom_qty, precision_digits=precision) == 1:
+                line.invoice_status = 'upselling'
+            elif float_compare(line.qty_invoiced, line.product_uom_qty, precision_digits=precision) >= 0:
+                line.invoice_status = 'invoiced'
+            else:
+                line.invoice_status = 'no'
+            print line.invoice_status
+
+
+    @api.depends('qty_invoiced', 'qty_delivered', 'product_uom_qty', 'order_id.state')
+    def _get_to_invoice_qty(self):
+        """
+        Compute the quantity to invoice. If the invoice policy is order, the quantity to invoice is
+        calculated from the ordered quantity. Otherwise, the quantity delivered is used.
+        """
+        for line in self:
+            if line.order_id.state in ['sale', 'done', 'partial_done']:
+                if line.product_id.invoice_policy == 'order':
+                    line.qty_to_invoice = line.product_uom_qty - line.qty_invoiced
+                else:
+                    line.qty_to_invoice = line.qty_delivered - line.qty_invoiced
+            else:
+                line.qty_to_invoice = 0
+
+
+
 
