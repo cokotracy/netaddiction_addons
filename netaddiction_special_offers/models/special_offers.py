ebf8c21329ba23f069366d85f2503bf1835ebb9c
andrea.bozzi <andreabozzi@macbookreabozzi.multiplayer.int>
2016-01-29 10:02:36 +0100

    tipi di offerta (prezzo,bundle..)

--- netaddiction_special_offers/models/special_offers.py
+++ netaddiction_special_offers/models/special_offers.py
@@ -21,6 +21,25 @@ class CatalogOffer(models.Model):
     qty_limit = fields.Integer( string='Quantità limite', help = "Quantità limiite di prodotti vendibili in questa offerta. 0 è illimitato", required=True)
     qty_min = fields.Integer( string='Quantità minima acquisto', help = "Quantità minima di prodotti da inserire nel carrello per attivare l'offerta.", required=True)
     qty_selled = fields.Integer( string='Quantità venduta', default=0)
+    offer_type = fields.Selection([(1,'Prezzo Fisso'),(2,'Percentuale')], string='Tipo Offerta', default=2,required=True)
+    fixed_price = fields.Integer(string="Prezzo fisso")
+    percent_discount = fields.Integer(string="Sconto Percentuale")
+
+    #BUG noto: se nella vista scrivi -1 su fixed price  poi cambi a percent discount riesci a scrivere il -1
+    #TODO:Non funziona bene nella vista, da sistemare
+    
+    @api.one
+    @api.constrains('fixed_price','offer_type')
+    def _check_value(self):
+        if self.offer_type == 1 and self.fixed_price <= 0:
+            raise ValidationError("Il valore del prezzo fisso non può essere minore  o uguale di zero")
+
+    @api.one
+    @api.constrains('percent_discount','offer_type')
+    def _check_value(self):
+        if self.offer_type == 2 and (self.percent_discount <= 0 or self.percent_discount > 100):
+            raise ValidationError("Il valore dello sconto percentuale non può essere minore di 0 o maggiore di 100")
+
 
     @api.one
     @api.constrains('date_start', 'date_end')
@@ -67,6 +86,16 @@ class ShoppingCartOffer(models.Model):
     date_end = fields.Date('End Date', help="Data di fine dell'offerta", required=True)
     list_prod = fields.One2many(comodel_name='product.product',compute='populate_products')
     priority = fields.Selection([(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9'),(10,'10')], string='Priorità', default=1,required=True)
+    qty_max_buyable = fields.Integer( string='Quantità massima acquistabile', help = "Quantità massima di prodotti acquistabili in unavendibili in questa offerta. 0 è illimitato", required=True)
+    qty_limit = fields.Integer( string='Quantità limite', help = "Quantità limiite di prodotti vendibili in questa offerta. 0 è illimitato", required=True)
+    qty_min = fields.Integer( string='Quantità minima acquisto', help = "Quantità minima di prodotti da inserire nel carrello per attivare l'offerta.", required=True)
+    qty_selled = fields.Integer( string='Quantità venduta', default=0)
+    offer_type = fields.Selection([(1,'Bundle'),(2,'n x m')], string='Tipo Offerta', default=2,required=True)
+    n = fields.Integer(string="N")
+    m = fields.Integer(string="M")
+
+    #TODO: N < M, N>0, M>0
+    
 
 
     @api.one
