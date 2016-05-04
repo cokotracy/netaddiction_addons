0c02a26c544863e8a344d470cfff7177917b168e
andrea.bozzi <andreabozzi@MacBook-di-andreabozzi.local>
2016-05-04 17:37:05 +0200

    paypal express checkout

--- netaddiction_payments/models/paypal_configuration.py
+++ netaddiction_payments/models/paypal_configuration.py
@@ -5,55 +5,42 @@ from Crypto import Random
 from openerp import models, fields, api
 import cypher
 
-BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
-unpad = lambda s : s[:-ord(s[len(s)-1:])]
+
 
 class PaypalConfiguration(models.TransientModel):
+    
     _name = "netaddiction.paypal.settings"
     _inherit = 'res.config.settings'
 
     username = fields.Char(string='Username', required=True, help="Paypal Username")
     password = fields.Char(string='password', required=True, help="Paypal Password")
     signature = fields.Char(string='Signature', required=True, help="The Paypal secret key")
-    email = fields.Char(string='Email', required=True, help="Paypal email")
+    
    
 
     
 
     @api.one
     def set_username(self, values):
-        print "-----USERNAME----"
         key = self.env["ir.config_parameter"].search([("key","=","paypal.key")]).value
         res = cypher.encrypt(key,self.username)
-        print res
         self.env["ir.values"].search([("name","=","paypal_username")]).value = res
 
 
     @api.one
     def set_password(self, values):
-        print "-----PASSWORD----"
         key = self.env["ir.config_parameter"].search([("key","=","paypal.key")]).value
         res = cypher.encrypt(key,self.password)
-        print res
         self.env["ir.values"].search([("name","=","paypal_password")]).value = res
         
 
     @api.one
     def set_signature(self, values):
-        print "-----SIGNATURE----"
         key = self.env["ir.config_parameter"].search([("key","=","paypal.key")]).value
         res = cypher.encrypt(key,self.signature)
-        print res
         self.env["ir.values"].search([("name","=","paypal_signature")]).value = res
         
 
-    @api.one
-    def set_email(self, values):
-        print "-----EMAIL----"
-        key = self.env["ir.config_parameter"].search([("key","=","paypal.key")]).value
-        res = cypher.encrypt(key,self.email)
-        print res
-        self.env["ir.values"].search([("name","=","paypal_email")]).value = res
+
         
         
--- netaddiction_payments/models/paypal_executor.py
+++ netaddiction_payments/models/paypal_executor.py
@@ -8,7 +8,12 @@ import paypal
 PAYMENTACTION = 'Sale'
 CUR = 'EUR'
 
+
+
 class PaypalExecutor(models.TransientModel):
+    """Classe di utilità associata a un transient model per effettuare e registrare
+    pagamenti con PayPal
+    """
     _name = "netaddiction.paypal.executor"
 
     token = fields.Char()
@@ -18,6 +23,18 @@ class PaypalExecutor(models.TransientModel):
 
     @api.one
     def get_express_checkout_link(self,amount,returnurl,cancelurl,email):
+        """ Primo metodo da chiamare per effettuare un pagamento su paypal
+            Parametri:
+            -amount: quantità da pagare
+            -returnurl: url a cui reindirizzare l'utente in caso di successo nel pagamento expres_checkout
+            -cancelurl: url a cui reindirizzare l'utente in caso di fallimento nel pagamento expres_checkout
+            -email: email del cliente
+            Returns:
+            -se tutto ok: il link per l'expres_checkout
+            -se fallisce set_express_checkout: -2
+            -se fallisce get_express_checkout_details: -1
+            Raise PayPalError: propaga i raise dell'interfaccia di paypal
+        """
         encripted_username = self.env["ir.values"].search([("name","=","paypal_username")]).value
         encripted_password = self.env["ir.values"].search([("name","=","paypal_password")]).value
         encripted_signature = self.env["ir.values"].search([("name","=","paypal_signature")]).value
@@ -28,7 +45,7 @@ class PaypalExecutor(models.TransientModel):
         username = cypher.decrypt(key,encripted_username)
         password = cypher.decrypt(key,encripted_password)
         signature = cypher.decrypt(key,encripted_signature)
-        #email = "andrea.bozzi-buyer@netaddiction.it"
+       
 
         #TODO: API_ENVIRONMENT = 'PRODUCTION'
         config = paypal.PayPalConfig(API_USERNAME=username,
@@ -43,28 +60,36 @@ class PaypalExecutor(models.TransientModel):
             email=email, currencycode=CUR
         )
 
-        if setexp_response and setexp_response['ACK'] == 'Success':
+        if setexp_response and setexp_response.success:
 
-            print "------setexp_response--------"
-            print setexp_response
             self.token = setexp_response.token
             getexp_response = pp_interface.get_express_checkout_details(token=self.token)
 
-            if getexp_response and getexp_response['ACK'] == 'Success':
-                print "------getexp_response--------"
-                print getexp_response
-                redirect_url = pp_interface.generate_express_checkout_redirect_url(self.token)
-                print "------redirect_url--------"
-                print redirect_url
+            if getexp_response and getexp_response.success:
+    
+                return pp_interface.generate_express_checkout_redirect_url(self.token)
+                
             else:
                 #TODO: return error
-                pass
+                return -1
         else:
             #TODO: return error
-            pass
+            return -2
 
     @api.one
     def finalize_payment(self,amount,user_id,order_id):
+        """ Secondo metodo da chiamare per effettuare un pagamento su paypal
+            Parametri:
+            -amount: quantità da pagare
+            -user_id: id su odoo dell'utente che deve pagare
+            -order_id: id su odoo dell'ordine associato al pagamento
+            
+            Returns:
+            -se tutto ok: 1
+            -se fallisce do_express_checkout_payment: -2
+            -se fallisce get_express_checkout_details: -1
+            Raise PayPalError: propaga i raise dell'interfaccia di paypal
+        """
         encripted_username = self.env["ir.values"].search([("name","=","paypal_username")]).value
         encripted_password = self.env["ir.values"].search([("name","=","paypal_password")]).value
         encripted_signature = self.env["ir.values"].search([("name","=","paypal_signature")]).value
@@ -80,21 +105,26 @@ class PaypalExecutor(models.TransientModel):
                       API_SIGNATURE=signature)
         pp_interface = paypal.PayPalInterface(config= config)
         getexp_response = pp_interface.get_express_checkout_details(token=self.token)
-        if getexp_response and getexp_response['ACK'] == 'Success':
+        if getexp_response and getexp_response.success:
             print getexp_response
-            payer_id = getexp_response['PAYERID']
-            print "--------------FINALIZE------------"
-            payment_response = pp_interface.do_express_checkout_payment(token=self.token, amt=amount, paymentaction='Sale',payerid=payer_id,currencycode='EUR')
-            if payment_response and payment_response['ACK'] == 'Success':
-                #todo: save on odoo
-                self._register_payment(user_id,payment_response['AMT'],order_id)
-                pass
+
+            try:
+                payer_id = getexp_response.payerid
+            except AttributeError:
+                #se non c'è il payerid nella risposta vuol dire che il cliente non ha completato il pagamento
+                return -1
+
+            
+            payment_response = pp_interface.do_express_checkout_payment(token=self.token, amt=amount, paymentaction=PAYMENTACTION,payerid=payer_id,currencycode=CUR)
+            
+            if payment_response and payment_response.success:
+                #save on odoo
+                return self._register_payment(user_id,payment_response.amt,order_id)
+                
             else:
-                #TODO: return error
-                pass
+                return -2
         else:
-            #TODO: return error
-            pass
+            return -1
 
 
     @api.one
@@ -102,12 +132,13 @@ class PaypalExecutor(models.TransientModel):
         pp_aj = self.env["account.journal"].search([("name","=","Paypal")])
         pay_inbound = self.env["account.payment.method"].search([("payment_type","=","inbound")])
         pay_inbound = pay_inbound[0] if isinstance(pay_inbound,list) else pay_inbound
-        print "HARE"
         if pp_aj and pay_inbound:
-            name = self.env['ir.sequence'].with_context(ir_sequence_date=fields.Date.context_today).next_by_code('account.payment.customer.invoice')
+            name = self.env['ir.sequence'].with_context(ir_sequence_date=fields.Date.context_today(self)).next_by_code('account.payment.customer.invoice')
             pp_id = pp_aj.id
             self.env["account.payment"].create({"partner_type" : "customer", "partner_id" : user_id, "journal_id" : pp_id, "amount" : amount, "order_id" : order_id, "state" : 'posted', "payment_type" : 'inbound', "payment_method_id" : pay_inbound.id, "name" : name })
-            print "HERE"
+            return 1
+        else:
+            return None
         
 
 
