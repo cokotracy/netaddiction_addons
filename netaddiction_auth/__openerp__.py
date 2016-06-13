5b543c8b1bc8d4a29bb6b13c68fd857fcbc76291
Yuri Farina <yuri.farina@multiplayer.it>
2016-06-13 18:08:50 +0200

    NetAddiction Auth

---
+++ netaddiction_auth/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "NetAddiction Auth",
+    'summary': "Implementa il vecchio sistema di autenticazione di multiplayer.com per consentire il passaggio al nuovo",
+    'description': """""",
+    'author': "NetAddiction",
+    'website': "http://www.netaddiction.it",
+    'category': 'Extra Tools',
+    'depends': ['base', 'auth_crypt'],
+    'version': '0.1',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
