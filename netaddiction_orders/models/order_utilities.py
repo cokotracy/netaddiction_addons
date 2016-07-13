cf04b54a653ad1c459409bff9d09feee0fafdac2
Yuri Farina <yuri.farina@multiplayer.it>
2016-07-13 15:55:18 +0200

    Refactoring get_cart

--- netaddiction_orders/models/order_utilities.py
+++ netaddiction_orders/models/order_utilities.py
@@ -1,77 +1,93 @@
 # -*- coding: utf-8 -*-
-from openerp import tools
-from openerp import models, fields, api
+
+from openerp import models
+
 
 class OrderUtilities(models.TransientModel):
-
     _name = "netaddiction.order.utilities"
 
-
-    @api.one
-    def get_cart(self,user_id=False, order_id=False):
+    def get_cart(self, partner_id=None, order_id=None, create=True):
         """
-        Restiusce il carrello dell'utente(ordine in draft) identificato da user_id, se non esiste lo crea.
+        Restiusce il carrello dell'utente (ordine in draft) identificato da partner_id, se non esiste lo crea.
         Se l'utente non esiste ritorna False
         """
-        if order:
-            return order if != draft
+        order_model = self.env['sale.order']
 
-        if user:
-            return last order if != draft
+        if order_id is not None:
+            order = order_model.search([('id', '=', order_id)])
 
-        create order for public user
-        if order_id :
-            order = self.env["sale.order"].search([("id","=",order_id)])
-            if order.state == 'draft':
+            if order and order.state == 'draft':
                 return order
-            else:
-                user_id =False
 
-        if not user_id:
-            #get user
-            pub_user_id = self.env['ir.model.data'].get_object('netaddiction_orders', 'public_user').id
-            return self.env["sale.order"].create({"partner_id":pub_user_id, "state":"draft"}) 
-            
-        usr = self.env["res.partner"].search([("id","=",user_id)])
-        if not usr:
-            pub_user_id = self.env['ir.model.data'].get_object('netaddiction_orders', 'public_user').id
-            return self.env["sale.order"].create({"partner_id":pub_user_id, "state":"draft"})
+        if partner_id is not None:
+            partner = self.env['res.partner'].search([('id', '=', partner_id)])
 
-        if len(usr.sale_order_ids) > 0:
-            ord_lst = self.env["sale.order"].search([("partner_id","=",user_id)],order="create_date desc")
-            if ord_lst and ord_lst[0].state == "draft":
-                return ord_lst[0]
+            if not partner:
+                raise Exception('Partner not found')
 
-        return self.env["sale.order"].create({"partner_id":user_id, "state":"draft"})
+            orders = order_model.search([('partner_id', '=', partner_id)], order='create_date desc')
 
+            if orders and orders[0].state == 'draft':
+                return orders[0]
 
-    @api.one
-    def add_to_cart(self,user_id,order_id,product_id,qty,bonus_list=None):
+        if create:
+            cart = order_model.create({
+                'partner_id': partner_id or self.env.ref('base.public_user_res_partner').id,
+                'state': 'draft',
+            })
+
+            return cart
+
+        return None
+
+    def add_to_cart(self, order_id, product_id, quantity, partner_id=None, bonus_list=None):
         """
         Aggiunge un prodotto al carrello di un utente
         Parametri:
-        - user_id id dell'utente
+        - partner_id id dell'utente
         - order_id id del carrello dell'utente
         - product_id id del prodotto da aggiungere
-        - qty quantità del prodotto da aggiungere
+        - quantity quantità del prodotto da aggiungere
         - bonus_list = [(bonus_id, bonus_qty)]  bonus_id id del prodotto bonus, bonus_qty quantità del prodotto bonus
         Return:
         - true se è andato tutto bene
         - false altrimenti
 
-        NB: non viene fatto alcun controllo sul fatto che i bonus siano effettivamente bonus del prodotto user_id
+        NB: non viene fatto alcun controllo sul fatto che i bonus siano effettivamente bonus del prodotto partner_id
         """
-        order = self.env["sale.order"].search([("id","=",order_id)])
-        prod = self.env["product.product"].search([("id","=",product_id)])
-        if order and order.partner_id.id == user_id and order.state == "draft" and prod:
-            ol = self.env["sale.order.line"].create({"order_id":order_id,"product_id":product_id,"product_uom_qty":qty, "product_uom":prod.uom_id.id,"name":prod.display_name})
+        if partner_id is None:
+            partner_id = self.env.ref('base.public_user_res_partner').id
+
+        order = self.env["sale.order"].search([("id", "=", order_id)])
+        prod = self.env["product.product"].search([("id", "=", product_id)])
+
+        if order and order.partner_id.id == partner_id and order.state == "draft" and prod:
+            ol = self.env["sale.order.line"].create({
+                "order_id": order_id,
+                "product_id": product_id,
+                "product_uom_qty": quantity,
+                "product_uom": prod.uom_id.id,
+                "name": prod.display_name,
+            })
+
             ol.product_id_change()
+
             if bonus_list:
                 for bonus_id, bonus_qty in bonus_list:
-                    bonus_prod = self.env["product.product"].search([("id","=",bonus_id)])
+                    bonus_prod = self.env["product.product"].search([("id", "=", bonus_id)])
+
                     if bonus_prod:
-                        ol_bonus = self.env["sale.order.line"].create({"order_id":order_id,"product_id":bonus_id,"product_uom_qty":bonus_qty,"bonus_father_id":ol.id,"product_uom":bonus_prod.uom_id.id,"name":bonus_prod.display_name})
+                        ol_bonus = self.env["sale.order.line"].create({
+                            "order_id": order_id,
+                            "product_id": bonus_id,
+                            "product_uom_qty": bonus_qty,
+                            "bonus_father_id": ol.id,
+                            "product_uom": bonus_prod.uom_id.id,
+                            "name": bonus_prod.display_name,
+                        })
+
                         ol_bonus.product_id_change()
+
             return True
-        else:
-            return False
+
+        return False
