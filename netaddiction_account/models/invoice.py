f507fddd2df2603a95fbee54e55057801b41b422
Matteo Piciucchi <matteo@localmatteo.com>
2016-07-30 15:46:11 +0200

    FIX invoice,viste autopreparazione e controllo quantità prodotto

--- netaddiction_account/models/invoice.py
+++ netaddiction_account/models/invoice.py
@@ -21,16 +21,18 @@ class Invoice(models.Model):
     @api.multi
     def write(self,values):
     	#se viene flaggato o deflaggato is_customer_invoice e il journal_id è = 1 (identifica fattura cliente)
-        if 'is_customer_invoice' in values and self.journal_id.id == 1:
+        if 'is_customer_invoice' in values:
         	#se non c'è il fiscalcode allora rimando un errore e rimetto il vecchio nome fattura e is_customer_invoci è False
         	#altrimenti procedo al cambio di nome
             if self.partner_id.parent_id.fiscalcode != False:
-                if values['is_customer_invoice']:
-                    values['number'] = self.number.strip() + '.1'
-                else:
-                    values['number'] = self.number.replace('.1','')
+                if values['number']:
+                    if values['is_customer_invoice']:
+                        values['number'] = self.number.strip() + '.1'
+                    else:
+                        values['number'] = self.number.replace('.1','')
             else:
-                values['number'] = self.number.replace('.1','')
+                if values['number']:
+                    values['number'] = self.number.replace('.1','')
                 values['is_customer_invoice'] = False
                 raise ValidationError('Il Cliente non ha un codice Fiscale')
 
