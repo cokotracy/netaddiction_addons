eed63080c34f114902918d34e9ceea678d824062
Matteo Piciucchi <matteo@localmatteo.com>
2016-08-14 11:35:26 +0200

    ordini b2b, tolta la constrains, ora c'è l'onchange

--- netaddiction_b2b/models/orders.py
+++ netaddiction_b2b/models/orders.py
@@ -27,14 +27,28 @@ class Orders(models.Model):
         if not self.is_b2b:
             return super(Orders,self).simulate_delivery_price(subdivision)
 
-    @api.constrains('partner_id','pricelist_id')
-    def set_b2b(self):
+    #@api.constrains('partner_id','pricelist_id')
+    #def set_b2b(self):
+    #    if self.partner_id.is_b2b:
+    #        #self.is_b2b = self.partner_id.is_b2b
+    #        # self.delivery_option = 'asap'
+    #        if self.partner_id.property_delivery_carrier_id:
+    #            self.carrier_id = self.partner_id.property_delivery_carrier_id
+    #        # self.payment_method_id = self.partner_id.favorite_payment_method
+
+    @api.onchange('partner_id')
+    def _get_partner_data_b2b(self):
         if self.partner_id.is_b2b:
-            #self.is_b2b = self.partner_id.is_b2b
-            # self.delivery_option = 'asap'
+            self.delivery_option = 'asap'
+            self.is_b2b = self.partner_id.is_b2b
             if self.partner_id.property_delivery_carrier_id:
                 self.carrier_id = self.partner_id.property_delivery_carrier_id
-            # self.payment_method_id = self.partner_id.favorite_payment_method
+            self.payment_method_id = self.partner_id.favorite_payment_method
+        else:
+            self.delivery_option = ''
+            self.is_b2b = self.partner_id.is_b2b
+            self.payment_method_id = False
+            self.carrier_id = False
 
 
     @api.multi
