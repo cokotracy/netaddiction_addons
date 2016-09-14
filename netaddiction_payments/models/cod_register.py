5a226a70fb7c8fb725e5c981cfe01e4044f2e1e0
andrea.bozzi <andreabozzi@macbookreabozzi.multiplayer.int>
2016-09-14 15:31:23 +0200

    adattamenti registro contrassegni a nuovi file sda e brt

--- netaddiction_payments/models/cod_register.py
+++ netaddiction_payments/models/cod_register.py
@@ -6,14 +6,14 @@ from float_compare import isclose
 from openerp import models, fields, api
 from openerp.exceptions import Warning
 
-BRT = "Numero riferimento spedizione"
-SDA = "Riferimenti"
+SDA = "Numero riferimento spedizione"
+BRT = "Riferimenti"
 MONEY_BRT = "Euro"
 MONEY_SDA = "Importo contrassegno"
 
 
 def strip_keys(d):
-# funzione di utilità che per effettuare lo stip delle chiavi di un dizionario
+    # funzione di utilità che per effettuare lo stip delle chiavi di un dizionario
     return {k.strip(): strip_keys(v)
             if isinstance(v, dict)
             else v
@@ -25,7 +25,7 @@ class CoDRegister(models.TransientModel):
 
     csv_file = fields.Binary('File')
 
-    def set_order_cash_on_delivery(self,order_id,real_invoice =False):
+    def set_order_cash_on_delivery(self, order_id, real_invoice=False):
         """ imposta l'ordine con id 'order_id' per essere pagato con contrassegno se l'ordine è in draft (bozza) o in sale (lavorazione).
             Crea una fattura e un pagamento per ogni spedizione. Aggiunge spese di contrassegno.
         """
@@ -93,15 +93,13 @@ class CoDRegister(models.TransientModel):
 
         return False
 
-
-
-    def _set_order_to_invoice(self,stock_move,order):
+    def _set_order_to_invoice(self, stock_move, order):
         """dato 'order' imposta qty_to_invoice alla quantità giusta solo per i prodotti che si trovano in 'stock_move'
         """
         prod_id = stock_move.product_id
         qty = stock_move.product_uom_qty
 
-        lines = [line for line in order.order_line if line.product_id == prod_id ]
+        lines = [line for line in order.order_line if line.product_id == prod_id]
         for line in lines:
             qty_to_invoice = qty if qty < line.product_uom_qty else line.product_uom_qty
 
@@ -112,8 +110,7 @@ class CoDRegister(models.TransientModel):
             if qty <= 0:
                 break
 
-
-    def set_delivery_to_invoice(self,pick,order,cod_id):
+    def set_delivery_to_invoice(self, pick, order, cod_id):
         """dato 'order' imposta qty_to_invoice per una spedizione e un contrassegno
         """
         lines = [line for line in order.order_line if line.is_delivery and line.price_unit == pick.carrier_price and  line.qty_invoiced < line.product_uom_qty]
@@ -126,46 +123,54 @@ class CoDRegister(models.TransientModel):
         if lines:
             lines[0].qty_to_invoice = 1
 
-
     @api.multi
     def execute(self):
         if self.csv_file:
             decoded64 = base64.b64decode(self.csv_file)
             decodedIO = io.BytesIO(decoded64)
             reader = csv.DictReader(decodedIO, delimiter=';')
-            #implementing the head-tail design pattern
+            # implementing the head-tail design pattern
             head = reader.next()
             head = strip_keys(head)
-
-            key = BRT if BRT in head else SDA
+            is_brt = True if BRT in head else False
+            key = BRT if is_brt else SDA
             money_key = MONEY_BRT if MONEY_BRT in head else MONEY_SDA
+
             warning_list = []
             contrassegno = self.env['ir.model.data'].get_object('netaddiction_payments', 'contrassegno_journal')
-            self._check_line(head,warning_list,key,money_key,contrassegno)
+
+            self._check_line(head, warning_list, key, money_key, contrassegno, is_brt)
 
             for line in reader:
                 # attenzione alle ultime due righe coi totali
                 line = strip_keys(line)
-                self._check_line(line,warning_list,key,money_key,contrassegno)
+
+                self._check_line(line, warning_list, key, money_key, contrassegno, is_brt)
 
             if warning_list:
-                raise Warning("non sono stati trovati pagamenti in contrassegno per i seguenti ordini nel file: %s" %warning_list)
+                raise Warning("non sono stati trovati pagamenti in contrassegno per i seguenti ordini nel file: %s" % warning_list)
 
-    def _check_line(self,line, warning_list,key,money_key,contrassegno):
+    def _check_line(self, line, warning_list, key, money_key, contrassegno, is_brt):
         found = False
-        if line[key]:
-            order = self.env["sale.order"].search([("name", "=", line[key])])
+        if line[key] and line[money_key]:
+            order = None
+            if is_brt:
+                order = self.env["sale.order"].search([("id", "=", line[key])])
+            else:
+                pick = self.env["stock.picking"].search([("id", "=", line[key])])
+                order = pick.sale_id if pick else None
             if order:
                 amount_str = line[money_key].replace(",", ".").replace("€", "")
                 amount = float(amount_str)
+
                 for payment in order.account_payment_ids:
 
-                    if (isclose(payment.amount, amount)) and payment.journal_id.id == contrassegno.id and not payment.state == 'posted':
+                    if (isclose(payment.amount, amount, abs_tol=0.009)) and payment.journal_id.id == contrassegno.id and not payment.state == 'posted':
                         payment.post()
                         found = True
                         break
                 if not found:
-                    warning_list.append((line[key], amount_str))
+                    warning_list.append((order.name, line[key], amount_str))
                 else:
                     all_paid = True
                     for p in order.account_payment_ids:
