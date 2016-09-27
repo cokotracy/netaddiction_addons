6ae358c437d56f9085805992e275e8f7190f1161
andrea.bozzi <andreabozzi@macbookreabozzi.multiplayer.int>
2016-09-27 11:06:53 +0200

    fix per fatture zero payment

--- netaddiction_payments/models/zeropayment_executor.py
+++ netaddiction_payments/models/zeropayment_executor.py
@@ -2,7 +2,7 @@
 
 
 from float_compare import isclose
-from openerp import api, models, fields
+from openerp import models
 
 import payment_exception
 
@@ -13,52 +13,53 @@ class ZeroPaymentExecutor(models.TransientModel):
     """
     _name = "netaddiction.zeropayment.executor"
 
-
-    def set_order_zero_payment(self,order, real_invoice=False):
+    def set_order_zero_payment(self, order, real_invoice=False):
         if not order:
-            raise payment_exception.PaymentException(payment_exception.ZERO,"order id non valido! %s" %order.id)
+            raise payment_exception.PaymentException(payment_exception.ZERO, "order id non valido! %s" % order.id)
 
-        if not isclose(order.amount_total,0.0000,abs_tol=0.009):
-            raise payment_exception.PaymentException(payment_exception.ZERO,"ordine non a 0! pagamento dovuto: % s" % order.amount_total)
+        if not isclose(order.amount_total, 0.0000, abs_tol=0.009):
+            raise payment_exception.PaymentException(payment_exception.ZERO, "ordine non a 0! pagamento dovuto: % s" % order.amount_total)
 
-        zeropayment_journal  = self.env['ir.model.data'].get_object('netaddiction_payments','zeropay_journal')
+        zeropayment_journal = self.env['ir.model.data'].get_object('netaddiction_payments', 'zeropay_journal')
         order.payment_method_id = zeropayment_journal.id
 
         if order.state == 'draft':
             order.action_confirm()
 
         if order.state == 'sale':
-            
-            if not isclose(order.amount_total,0.0000,abs_tol=0.009):
-                #TODO spostare in problema e commentare?
-                raise payment_exception.PaymentException(payment_exception.ZERO,"ordine non a 0! pagamento dovuto di spedizione: % s" % order.amount_total)
+
+            if not isclose(order.amount_total, 0.0000, abs_tol=0.009):
+                # TODO spostare in problema e commentare?
+                raise payment_exception.PaymentException(payment_exception.ZERO, "ordine non a 0! pagamento dovuto di spedizione: % s" % order.amount_total)
 
             inv_lst = []
 
-
             for line in order.order_line:
-                #resetto la qty_to_invoice di tutte le linee
+                # resetto la qty_to_invoice di tutte le linee
                 line.qty_to_invoice = 0
-            for delivery in order.picking_ids:                    
+            for delivery in order.picking_ids:
                 for stock_move in delivery.move_lines_related:
-                    self._set_order_to_invoice(stock_move,order)
+                    self._set_order_to_invoice(stock_move, order)
 
-                self._set_delivery_to_invoice(delivery,order)
+                self._set_delivery_to_invoice(delivery, order)
 
                 inv_lst += order.action_invoice_create()
 
             for inv in inv_lst:
-                invoice = self.env['account.invoice'].search([("id","=",inv)])
+                invoice = self.env['account.invoice'].search([("id", "=", inv)])
                 invoice.signal_workflow('invoice_open')
+                if order.gift_discount > 0.0:
+                    gift_value = self.env["netaddiction.gift_invoice_helper"].compute_gift_value(order.gift_discount, order.amount_total, invoice.amount_total)
+                    self.env["netaddiction.gift_invoice_helper"].gift_to_invoice(gift_value, invoice)
                 invoice.is_customer_invoice = real_invoice
 
-    def _set_order_to_invoice(self,stock_move,order):
+    def _set_order_to_invoice(self, stock_move, order):
         """dato 'order' imposta qty_to_invoice alla quantità giusta solo per i prodotti che si trovano in 'stock_move'
         """
         prod_id = stock_move.product_id
         qty = stock_move.product_uom_qty
 
-        lines = [line for line in order.order_line if line.product_id == prod_id ]
+        lines = [line for line in order.order_line if line.product_id == prod_id]
         for line in lines:
             qty_to_invoice = qty if qty < line.product_uom_qty else line.product_uom_qty
 
@@ -68,11 +69,11 @@ class ZeroPaymentExecutor(models.TransientModel):
 
             if qty <= 0:
                 break
-
-    def _set_delivery_to_invoice(self,pick,order):
+ 
+    def _set_delivery_to_invoice(self, pick, order):
         """dato 'order' imposta qty_to_invoice per una spedizione 
         """
-        lines = [line for line in order.order_line if line.is_delivery and line.price_unit == pick.carrier_price and  line.qty_invoiced < line.product_uom_qty]
+        lines = [line for line in order.order_line if line.is_delivery and line.price_unit == pick.carrier_price and line.qty_invoiced < line.product_uom_qty]
 
         if lines:
             lines[0].qty_to_invoice = 1
