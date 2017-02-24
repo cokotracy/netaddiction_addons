6156df1911326d9d8f0620ba7b5657fc06c6b4ff
andrea.bozzi <andreabozzi@macbookreabozzi.multiplayer.int>
2017-02-24 10:15:52 +0100

    fix in mail mandate per offerte carrello

--- netaddiction_mail/models/offer.py
+++ netaddiction_mail/models/offer.py
@@ -26,7 +26,7 @@ class OfferCartLine(models.Model):
     @api.one
     @api.constrains('active')
     def _check_active(self):
-        if not self.active and self.offer_catalog_id.active and self.qty_limit > 0 and self.qty_selled >= self.qty_limit:
+        if not self.active and self.offer_cart_id.active and self.qty_limit > 0 and self.qty_selled >= self.qty_limit:
             # TODO risistemare acl per mail o fare menu di configurazione
             # users = self.env["netaddiction.email.dispatcher"].get_users_from_group("netaddiction_acl.netaddiction_products_data_entry_user_manager")
             # users += self.env["netaddiction.email.dispatcher"].get_users_from_group("netaddiction_acl.netaddiction_sale_user_manager")
