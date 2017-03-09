8d4eee0f2c052f6f470de2fb65955de13bfca28b
andrea.bozzi <andreabozzi@macbookreabozzi.multiplayer.int>
2017-03-09 10:34:38 +0100

    controllo categorie spegnimento

--- netaddiction_mail/models/product.py
+++ netaddiction_mail/models/product.py
@@ -13,10 +13,17 @@ class Products(models.Model):
         if self.env.context.get('skip_notification_mail', False):
             return
 
-        if not self.sale_ok:
-            # TODO risistemare acl per mail o fare menu di configurazione
-            # users = self.env["netaddiction.email.dispatcher"].get_users_from_group("netaddiction_acl.netaddiction_products_data_entry_user_manager")
-            # users += self.env["netaddiction.email.dispatcher"].get_users_from_group("netaddiction_acl.netaddiction_sale_user_manager")
+        # if not self.sale_ok:
+        included_categories = []
+        included_categories.append(self.env["product.category"].search([("name", "=", "Videogiochi")]).id)
+        included_categories.append(self.env["product.category"].search([("name", "=", "Gadget")]).id)
+        included_categories.append(self.env["product.category"].search([("name", "=", "Figures")]).id)
+        included_categories.append(self.env["product.category"].search([("name", "=", "Modellismo e Model Kit")]).id)
+
+        # TODO risistemare acl per mail o fare menu di configurazione
+        # users = self.env["netaddiction.email.dispatcher"].get_users_from_group("netaddiction_acl.netaddiction_products_data_entry_user_manager")
+        # users += self.env["netaddiction.email.dispatcher"].get_users_from_group("netaddiction_acl.netaddiction_sale_user_manager")
+        if self.categ_id.id in included_categories:
             obj = "[SHOPPING] PRODOTTO ESAURITO [%s] %s id: %s" % (self.categ_id.name, self.name, self.id)
             users_2 = "andrea.alunni@netaddiction.it, riccardo.ioni@netaddiction.it"
             # self.env["netaddiction.email.dispatcher"].send_notification(obj, obj, "shopping@multiplayer.com", set(users))
@@ -30,13 +37,19 @@ class Products(models.Model):
         # TODO risistemare acl per mail o fare menu di configurazione
         # users = self.env["netaddiction.email.dispatcher"].get_users_from_group("netaddiction_acl.netaddiction_products_data_entry_user_manager")
         # users += self.env["netaddiction.email.dispatcher"].get_users_from_group("netaddiction_acl.netaddiction_sale_user_manager")
-        users_2 = "andrea.alunni@netaddiction.it, riccardo.ioni@netaddiction.it"
-        if self.visible:
-            obj = "[SHOPPING] PRODOTTO ACCESO [%s] %s id: %s" % (self.categ_id.name, self.name, self.id)
-        else:
-            obj = "[SHOPPING] PRODOTTO SPENTO [%s] %s id: %s" % (self.categ_id.name, self.name, self.id)
-        # self.env["netaddiction.email.dispatcher"].send_notification(obj, obj, "shopping@multiplayer.com", set(users))
-        self.env["netaddiction.email.dispatcher"].send_mail_fixed_recipients(obj, obj, "shopping@multiplayer.com", users_2)
+        included_categories = []
+        included_categories.append(self.env["product.category"].search([("name", "=", "Videogiochi")]).id)
+        included_categories.append(self.env["product.category"].search([("name", "=", "Gadget")]).id)
+        included_categories.append(self.env["product.category"].search([("name", "=", "Figures")]).id)
+        included_categories.append(self.env["product.category"].search([("name", "=", "Modellismo e Model Kit")]).id)
+        if self.categ_id.id in included_categories:
+            users_2 = "andrea.alunni@netaddiction.it, riccardo.ioni@netaddiction.it"
+            if self.visible:
+                obj = "[SHOPPING] PRODOTTO ACCESO [%s] %s id: %s" % (self.categ_id.name, self.name, self.id)
+            else:
+                obj = "[SHOPPING] PRODOTTO SPENTO [%s] %s id: %s" % (self.categ_id.name, self.name, self.id)
+            # self.env["netaddiction.email.dispatcher"].send_notification(obj, obj, "shopping@multiplayer.com", set(users))
+            self.env["netaddiction.email.dispatcher"].send_mail_fixed_recipients(obj, obj, "shopping@multiplayer.com", users_2)
 
     @api.model
     def _verify_release_date(self):
