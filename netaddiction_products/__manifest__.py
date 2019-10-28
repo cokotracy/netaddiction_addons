1782d66ace485c85acba2710b2ecc9be5df0a782
Francesco Apruzzese <francescoapruzzese@openforce.it>
2019-10-28 10:28:41 +0100

    [ADD] [netaddiction_products] New module

---
+++ netaddiction_products/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Openforce Srls Unipersonale (www.openforce.it)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Netaddiction Products',
+    'version': '12.0.1.0.0',
+    'category': 'Product',
+    'author': 'Openforce',
+    'license': 'LGPL-3',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+}
