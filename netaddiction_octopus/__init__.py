04a81d4a85d60d9559e5881f0ac2cec13b5ae24a
Francesco Apruzzese <francescoapruzzese@openforce.it>
2019-11-12 12:46:45 +0100

    [ADD] [netaddiction_octopus] Blacklist model and security rules

--- netaddiction_octopus/__init__.py
+++ netaddiction_octopus/__init__.py
@@ -1,7 +1,1 @@
-# from models import blacklist
-# from models import category
-# from models import cron
-# from models import product
-# from models import supplier
-# from models import tax
-# from models import autoimport
+from . import models
--- netaddiction_octopus/models/blacklist.py
+++ netaddiction_octopus/models/blacklist.py
@@ -2,9 +2,20 @@ from openerp import models, fields
 
 
 class Blacklist(models.Model):
+
     _name = 'netaddiction_octopus.blacklist'
 
-    _sql_constraints = [('duplicate', 'unique(supplier_id, supplier_code)', 'This product is already blacklisted')]
+    _sql_constraints = [(
+        'duplicate',
+        'unique(supplier_id, supplier_code)',
+        'This product is already blacklisted')]
 
-    supplier_id = fields.Many2one('res.partner', string='Fornitore', domain=[('supplier', '=', True)])
-    supplier_code = fields.Char('Codice fornitore')
+    supplier_code = fields.Char(
+        string='Codice fornitore'
+    )
+
+    supplier_id = fields.Many2one(
+        'res.partner',
+        string='Fornitore',
+        domain=[('supplier', '=', True)]
+    )
