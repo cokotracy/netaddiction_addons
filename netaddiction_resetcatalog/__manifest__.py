23323b5df8d3c1b1a004a444d65c521d4487aff8
OrcoBot <orcobot@openforce.it>
2020-01-23 08:59:40 +0000

    netaddiction_resetcatalog 12.0.1.1.0

--- netaddiction_resetcatalog/__manifest__.py
+++ netaddiction_resetcatalog/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Netaddiction Reset Catalog',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'category': 'Product',
     'author': 'Openforce',
     'license': 'LGPL-3',
