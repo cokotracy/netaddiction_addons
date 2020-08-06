8a939a2cb75c14cbe78bc51b5cc39c9633633543
Ravi Singh <ras@odoo.com>
2020-08-06 18:27:55 +0530

    Homepage Layout

---
+++ netaddiction_website/__manifest__.py
@@ -0,0 +1,13 @@
+{
+	'name': 'Offshore App',
+	'depends': [
+		'base',
+        'website',
+    ],
+    'data': [
+    	'views/base_template.xml'
+    ],
+	'installable': True,
+    'application': True,
+    'auto_install': True,
+}
