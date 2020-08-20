4f386f9588334df5c8072ab2aed677764a237f14
jpr-odoo <jpr@openerp.com>
2020-08-20 18:40:59 +0530

    best sellet product domain fix

--- netaddiction_website/controllers/main.py
+++ netaddiction_website/controllers/main.py
@@ -24,7 +24,7 @@ class WebsiteSale(WebsiteSale):
         }
         rating = request.website.viewref('website_sale.product_comment').active
         res = {'products': []}
-        for product in request.env['product.product'].search([('net_sales_count', '>=', 0)], order="net_sales_count desc", limit=10):
+        for product in request.env['product.product'].search([('net_sales_count', '>', 0)], order="net_sales_count desc", limit=10):
             combination_info = product._get_combination_info_variant()
             res_product = product.read(['id', 'name', 'website_url'])[0]
             res_product.update(combination_info)
