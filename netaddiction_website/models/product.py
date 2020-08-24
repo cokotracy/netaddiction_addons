8410a47827598b02bcbe997d35ecbef666c882c0
jpr-odoo <jpr@openerp.com>
2020-08-24 14:48:08 +0530

    shop page price formate

--- netaddiction_website/models/product.py
+++ netaddiction_website/models/product.py
@@ -18,6 +18,22 @@ class ProductProduct(models.Model):
             product.net_sales_count = product.sales_count
 
 class ProductTemplate(models.Model):
-	_inherit = 'product.template'
+    _inherit = 'product.template'
 
-	est_date_addon = fields.Integer('Estimate date Addon',default= 5)
+    est_date_addon = fields.Integer('Estimate date Addon',default= 5)
+
+    def _get_netaddiction_combination_info(self, website, combination_info):
+        netaddiction_combination_info = combination_info
+        FieldMonetary = self.env['ir.qweb.field.monetary']
+        monetary_options = {
+            'display_currency': website.get_current_pricelist().currency_id,
+        }
+        list_price = FieldMonetary.value_to_html(combination_info['list_price'], monetary_options).split('.')
+        list_price_decimal = u'</span><span class="o_netaddiction_decimal">.' + list_price[1]
+        netaddiction_combination_info['netaddiction_list_price'] = list_price[0] + list_price_decimal
+
+        price_formate = FieldMonetary.value_to_html(combination_info['price'], monetary_options).split('.')
+        decimal = u'</span><span class="o_netaddiction_decimal">.' + price_formate[1]
+        netaddiction_combination_info['netaddiction_price'] = price_formate[0] + decimal
+        return netaddiction_combination_info
+
