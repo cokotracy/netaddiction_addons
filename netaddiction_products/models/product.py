449cbd9860e2f9c812fc837bbe377b4a28221116
Andrea Colangelo <andreacolangelo@openforce.it>
2020-08-25 12:09:46 +0200

    [ADD][netaddiction_products] Port from v.12

---
+++ netaddiction_products/models/product.py
@@ -0,0 +1,43 @@
+# Copyright 2019 Openforce Srls Unipersonale (www.openforce.it)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = 'product.template'
+
+    out_date = fields.Date()
+
+
+class ProductProduct(models.Model):
+
+    _inherit = 'product.product'
+
+    final_price = fields.Float(
+        string="Pricelist Price",
+        digits=dp.get_precision('Product Price')
+    )
+
+    qty_available_now = fields.Integer(
+        compute="_get_qty_available_now",
+        # search="_search_available_now",
+        string="Quantità Disponibile",
+        help="Quantità Disponibile Adesso (qty in possesso - qty in uscita)")
+
+    @api.multi
+    def _get_qty_available_now(self):
+        for product in self:
+            product.qty_available_now = \
+                product.qty_available - product.outgoing_qty
+
+
+class SupplierInfo(models.Model):
+
+    _inherit = 'product.supplierinfo'
+
+    avail_qty = fields.Float(
+        string='Available Qty'
+    )
