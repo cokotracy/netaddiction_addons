b16de18f24ad53ef8753f617d9d9c4e5e3ca9fb8
Andrea Colangelo <andreacolangelo@openforce.it>
2020-11-02 15:53:34 +0000

    [ADD][netaddiction_orders] Port from v12

---
+++ netaddiction_orders/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Openforce Srls Unipersonale (www.openforce.it)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Netaddiction Orders',
+    'version': '13.0.1.0.0',
+    'category': 'Sale',
+    'author': 'Openforce',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        # 'views/sale_views.xml',
+    ],
+}
