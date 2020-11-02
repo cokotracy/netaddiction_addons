42f20c567d79fd5c09a017bdd2f1fd453585a1fd
Andrea Colangelo <andreacolangelo@openforce.it>
2020-11-02 15:53:05 +0000

    [ADD][netaddiction_account] Port from v12

---
+++ netaddiction_account/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Openforce Srls Unipersonale (www.openforce.it)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Netaddiction Account',
+    'version': '13.0.1.0.0',
+    'category': 'Account',
+    'author': 'Openforce',
+    'license': 'LGPL-3',
+    'depends': [
+        'account',
+        'l10n_it_fiscalcode',
+    ],
+    'data': [
+#        'views/account_views.xml',
+    ],
+}
