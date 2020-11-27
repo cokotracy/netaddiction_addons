9ff2a7453e15fb36f3afe18c46ae237d205fcde5
Dev <dev@multiplayer.it>
2020-11-27 18:32:56 +0100

    Stripe checkout

--- netaddiction_payments/models/__init__.py
+++ netaddiction_payments/models/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-import account_payment, paypal_configuration, paypal_executor, positivity_executor, positivity_configuration, partner, cod_register, sale, payment_exception, zeropayment_executor, cash_executor, sofort_configuration, sofort_executor, gift_invoice
+import account_payment, paypal_configuration, paypal_executor, positivity_executor, positivity_configuration, stripe_executor, partner, cod_register, sale, payment_exception, zeropayment_executor, cash_executor, sofort_configuration, sofort_executor, gift_invoice
--- netaddiction_payments/models/partner.py
+++ netaddiction_payments/models/partner.py
@@ -10,27 +10,29 @@ class CCData(models.Model):
     _name = "netaddiction.partner.ccdata"
 
     _sql_constraints = [
-        ('token_unique', 'unique(token, active)', 'Esiste già una carta con questo token!'),
+        ('token_unique', 'unique(token, active)',
+         'Esiste già una carta con questo token!'),
     ]
 
-    active = fields.Boolean(string='Attivo', help="Permette di spengere la carta senza cancellarla", default=True)
+    active = fields.Boolean(
+        string='Attivo', help="Permette di spengere la carta senza cancellarla", default=True)
     default = fields.Boolean(string="Carta di default", default=False)
     token = fields.Char(string='Token', required=True)
     last_four = fields.Char(string='Indizio', required=True)
     month = fields.Integer(string='Mese', required=True)
     year = fields.Integer(string='Anno', required=True)
     name = fields.Char(string='Titolare', required=True)
-    customer_id = fields.Many2one('res.partner', string='Cliente', required=True)
+    customer_id = fields.Many2one(
+        'res.partner', string='Cliente', required=True)
     ctype = fields.Char(string='Tipo Carta', default="")
-    company_id = fields.Many2one('res.company', related='customer_id.company_id', store=True)
+    company_id = fields.Many2one(
+        'res.company', related='customer_id.company_id', store=True)
 
     @api.multi
     def name_get(self):
         res = []
-
         for s in self:
             res.append((s.id, ' '.join([s.name, s.last_four, s.ctype])))
-
         return res
 
     @api.constrains('default')
@@ -60,7 +62,8 @@ class CCData(models.Model):
     def unlink(self):
         for cc in self:
             if cc.active:
-                open_payments = self.env['account.payment'].search([("state", "=", "draft"), ("partner_id", "=", self.customer_id.id), ("cc_token", "=", self.token)])
+                open_payments = self.env['account.payment'].search(
+                    [("state", "=", "draft"), ("partner_id", "=", self.customer_id.id), ("cc_token", "=", self.token)])
                 if open_payments:
                     orders = ""
                     for payment in open_payments:
@@ -68,9 +71,18 @@ class CCData(models.Model):
                         orders += payment.order_id.name
                     raise CardWithOrdersException(self.token, orders)
 
+                # Rimuovo il token da BNL
                 try:
-                    self.env["netaddiction.positivity.executor"].token_delete(self.customer_id.id, self.token)
+                    self.env["netaddiction.positivity.executor"].token_delete(
+                        self.customer_id.id, self.token)
                 except Exception:
+                    pass
+
+                # Rimuovo il token da Stripe
+                try:
+                    self.env["netaddiction.stripe.executor"].token_delete(
+                        self.customer_id.email, self.token)
+                except Exception as e:
                     pass
         super(CCData, self).unlink()
 
@@ -78,7 +90,8 @@ class CCData(models.Model):
 class CCDataPartner(models.Model):
     _inherit = 'res.partner'
 
-    ccdata_id = fields.One2many('netaddiction.partner.ccdata', 'customer_id', string="Carta")
+    ccdata_id = fields.One2many(
+        'netaddiction.partner.ccdata', 'customer_id', string="Carta")
 
 
 class CardWithOrdersException(Exception):
@@ -89,9 +102,11 @@ class CardWithOrdersException(Exception):
         self.token = token
 
     def __str__(self):
-        s = u"Questa carta non puo' essere cancellata perche' connessa a ordini non completati : %s " % (self.err_str)
+        s = u"Questa carta non puo' essere cancellata perche' connessa a ordini non completati : %s " % (
+            self.err_str)
         return s
 
     def __repr__(self):
-        s = u"Questa carta non può essere cancellata perche' connessa a ordini non completati : %s " % (self.err_str)
+        s = u"Questa carta non può essere cancellata perche' connessa a ordini non completati : %s " % (
+            self.err_str)
         return s
