b8925ecc39427487a117bd119e626c34d7feb945
SilvioGregorini <silviogregorini@openforce.it>
2020-12-02 16:41:20 +0100

    [UPD][netaddiction_products] Lesser update (preparatory to netaddiction_warehouse migration)

--- netaddiction_products/models/product.py
+++ netaddiction_products/models/product.py
@@ -1,14 +1,37 @@
 # Copyright 2019 Openforce Srls Unipersonale (www.openforce.it)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-
     _inherit = 'product.template'
 
-    out_date = fields.Date()
+    available_date = fields.Date(
+        string="Data disponibilità"
+    )
+
+    out_date = fields.Date(
+        string="Data di Uscita"
+    )
+
+    out_date_approx_type = fields.Selection(
+        [('accurate', 'Preciso'),
+         ('month', 'Mensile'),
+         ('quarter', 'Trimestrale'),
+         ('four', 'Quadrimestrale'),
+         ('year', 'Annuale')],
+        help="Impatta sulla vista front end\n"
+        "Preciso: la data inserita è quella di uscita\n"
+        "Mensile: qualsiasi data inserita prende solo il mese e l'anno"
+        " (es: in uscita nel mese di Dicembre 2019)\n"
+        "Trimestrale: prende l'anno e mese e calcola il trimestre"
+         " (es:in uscita nel terzo trimestre 2019)\n"
+        "Quadrimestrale: prende anno e mese e calcola il quadrimestre"
+         " (es:in uscita nel primo quadrimestre del 2019)\n"
+        "Annuale: prende solo l'anno (es: in uscita nel 2019)",
+        string="Approssimazione Data",
+    )
 
 
 class ProductProduct(models.Model):
