f5198588be3b42bcef5ce1b9d39abe653b81a051
Andrea Colangelo <andreacolangelo@openforce.it>
2020-12-21 23:05:16 +0100

    [ADD][netaddiction_payments] Add a few fields not ported from v9.0 yet

---
+++ netaddiction_payments/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2020 Openforce Srls Unipersonale (www.openforce.it)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from . import sale_order
+from . import stock_picking
