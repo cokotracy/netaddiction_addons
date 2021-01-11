6c7daed3bfb1a6421727bb370a46990188a44322
Dario Del Zozzo <dariodelzozzo@openforce.it>
2021-01-11 13:40:25 +0100

    [ADD][netaddiction_warehouse] Migration of action client Carico Da Fornitore from 9.0 to 13.0]

--- netaddiction_warehouse/models/__init__.py
+++ netaddiction_warehouse/models/__init__.py
@@ -17,7 +17,7 @@ from . import sale_order
 from . import sale_order_line
 from . import stock_inventory
 from . import stock_move
-from . import stock_move_line
+# from . import stock_move_line
 from . import stock_picking
 from . import stock_picking_batch
 from . import stock_picking_type
