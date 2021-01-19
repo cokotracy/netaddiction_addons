53f6a30c3dff137f5e06f3839158498dba88e01f
Andrea Colangelo <andreacolangelo@openforce.it>
2021-01-19 18:04:57 +0000

    [FIX][netaddiction_products] Change the way default_code is stored as the current way is heavily bugged

--- netaddiction_products/models/product.py
+++ netaddiction_products/models/product.py
@@ -1,6 +1,8 @@
 # Copyright 2019 Openforce Srls Unipersonale (www.openforce.it)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
 from datetime import date, timedelta
+
 from odoo import api, fields, models
 
 
@@ -93,11 +95,8 @@ class ProductProduct(models.Model):
     _order = 'name, id'
 
     default_code = fields.Char(
-        string='Internal Reference',
-        compute='compute_default_code',
-        search='search_default_code',
         readonly=True,
-        index=True,
+        store=True,
     )
 
     final_price = fields.Float(
@@ -129,6 +128,13 @@ class ProductProduct(models.Model):
         compute="_get_inventory_medium_value"
     )
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        for product in res:
+            product.default_code = str(product.id)
+        return res
+
     def _get_inventory_medium_value(self):
         for product in self:
             stock = self.env.ref('stock.stock_location_stock').id
@@ -153,13 +159,6 @@ class ProductProduct(models.Model):
                     result['total_excluded'], 2)
             else:
                 product.med_inventory_value = 0
-
-    def compute_default_code(self):
-        for product in self:
-            product.default_code = str(product.id)
-
-    def search_default_code(self, operator, value):
-        return [('id', operator, value)]
 
     def _get_qty_available_now(self):
         for product in self:
