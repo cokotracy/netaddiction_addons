9d30eab05de3c96db382c04bd58c76617bc5f834
Francesco Apruzzese <cescoap@gmail.com>
2021-02-22 10:54:25 +0100

    [IMP] [netaddiction_b2b] Disable useless pricelists

--- netaddiction_b2b/__manifest__.py
+++ netaddiction_b2b/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2019 Openforce Srls Unipersonale (www.openforce.it)
+# Copyright 2019-TODAY Openforce Srls Unipersonale (www.openforce.it)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Netaddiction B2B',
-    'version': '13.0.2.0.0',
+    'version': '13.0.2.1.0',
     'category': 'Sale',
     'author': 'Openforce',
     'license': 'LGPL-3',
