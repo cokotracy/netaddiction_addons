af72bd20f40c85e9032c0014c30a6238d66a2834
OrcoBot <orcobot@openforce.it>
2021-04-29 14:21:16 +0000

    netaddiction_warehouse 13.0.4.2.0

--- netaddiction_warehouse/__manifest__.py
+++ netaddiction_warehouse/__manifest__.py
@@ -4,7 +4,7 @@
     'author': "NetAddiction",
     'website': "http://www.netaddiction.it",
     'category': 'Warehouse Management',
-    'version': '13.0.4.1.0',
+    'version': '13.0.4.2.0',
     'depends': [
         'base',
         'delivery',
