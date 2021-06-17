a74adfa80eefefdb791ef25b1d33dd80e462ad5b
Andrea Colangelo <andrea.colangelo@rapsodoo.com>
2021-06-17 09:38:00 +0000

    [IMP][scripts] Adapt mig_products.py to the new Odoo 14 instance

--- scripts/mig_products.py
+++ scripts/mig_products.py
@@ -1,29 +1,34 @@
 #! /usr/bin/env python3
 
-# Copyright 2021 Openforce Srls Unipersonale (www.openforce.it)
+# Copyright 2021 Rapsodoo Italia SrL (www.rapsodoo.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-# It looks like list_price is not properly set after the 9->13 migration of
+# It looks like list_price is not properly set after the 9->14 migration of
 # NetAddiction. This script retrieves the data from the existing Odoo 9
-# instance and restores the prices on Odoo 13
+# instance and restores the prices on Odoo 14
 
 
 import erppeek
+import ssl
 
-URL13 = 'http://localhost:2069'
-DB13 = 'netaddiction_migrazione'
-LOGIN13 = 'ecommerce-servizio@netaddiction.it'
-PASSWORD13 = 'b*Y^x#AR2D71'
+# The Odoo 9 has an invalid certificate sometimes. We are confident enough
+# in it to skip the certificate validation
+ssl._create_default_https_context = ssl._create_unverified_context
+
+URL14 = 'http://localhost:8069'
+DB14 = 'migrazione_netaddiction'
+LOGIN14 = 'ecommerce-servizio@netaddiction.it'
+PASSWORD14 = '2VBrhX^49Qh!'
 
 URL9 = 'https://backoffice-staging.netaddiction.it'
 DB9 = 'odoo'
 LOGIN9 = 'ecommerce-servizio@netaddiction.it'
-PASSWORD9 = 'b*Y^x#AR2D71'
+PASSWORD9 = '2VBrhX^49Qh!'
 
 
 def main():
     odoo9 = erppeek.Client(URL9, DB9, LOGIN9, PASSWORD9)
-    odoo13 = erppeek.Client(URL13, DB13, LOGIN13, PASSWORD13)
+    odoo14 = erppeek.Client(URL14, DB14, LOGIN14, PASSWORD14)
 
     print("Reading data on Odoo 9. This may take a while...")
     data9 = odoo9.read(
@@ -32,11 +37,11 @@ def main():
         ('id', 'list_price')
     )
 
-    product13 = odoo13.model('product.product')
+    product14 = odoo14.model('product.product')
     for item9 in data9:
         print(f"Processing product ID {item9['id']}")
         try:
-            product13.browse(item9['id']).list_price = item9['list_price']
+            product14.browse(item9['id']).list_price = item9['list_price']
         except Exception:
             print(f"ERROR: Skipping product ID {item9['id']}")
 
