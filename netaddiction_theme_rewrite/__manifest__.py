b6ce96b9e9dd7ca3b780e2b96fe79c7c6f66fd07
Fabio Filipponi <fabio.filipponi@netaddiction.it>
2021-07-05 11:52:01 +0200

    modifica versioning modulo

--- netaddiction_theme_rewrite/__manifest__.py
+++ netaddiction_theme_rewrite/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Netaddiction Theme Rewrite',
     'category': 'eCommerce',
-    'version': '1.0',
+    'version': '14.0.1.0.0',
     'author': 'Netaddiction',
     'license': 'LGPL-3',
     'depends': [
