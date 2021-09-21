75003d8ade747654510b70800eafaa998624ea64
Francesco Apruzzese <cescoap@gmail.com>
2021-09-21 15:23:18 +0200

    [FIX][netaddiction_warehouse] Fix broken manifest 'creation' and 'send to ftp' features

--- netaddiction_warehouse/__manifest__.py
+++ netaddiction_warehouse/__manifest__.py
@@ -4,7 +4,7 @@
     'author': "NetAddiction",
     'website': "http://www.netaddiction.it",
     'category': 'Warehouse Management',
-    'version': '14.0.1.2.0',
+    'version': '14.0.1.3.0',
     'depends': [
         'base',
         'delivery',
