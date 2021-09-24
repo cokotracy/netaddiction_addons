e399d8b4ff26afc5bc3dc43faf7d202987a017ad
Matteo <matteo.serafini@netaddiction.it>
2021-09-24 12:31:09 +0200

    Modulo: Notifiche email restock

--- netaddiction_website_stock_notify/models/product_stock.py
+++ netaddiction_website_stock_notify/models/product_stock.py
@@ -4,26 +4,26 @@ from odoo import models, fields
 
 _logger = logging.getLogger(__name__)
 
+
 class ProductStockNotification(models.Model):
-    #Private Attributes
-    _name = 'product.stock.notification'
+    # Private Attributes
+    _name = "product.stock.notification"
     _description = "Product Stock Notification"
 
     # -------------------
     # Fields Declarations
     # -------------------
-    product_id = fields.Many2one('product.product', string='Product')
-    email = fields.Char(string='Email')
-    state = fields.Selection([('draft', 'Draft'), ('sent', 'Sent')], string='State', default='draft')
+    product_id = fields.Many2one("product.product", string="Product")
+    email = fields.Char(string="Email")
+    state = fields.Selection([("draft", "Draft"), ("sent", "Sent")], string="State", default="draft")
 
     def cron_product_stock_notification_check(self):
-        """This method will call from cron and will notify subscriber if product gets back into the stock 
-        """
-        for record in self.search([('state', '=', 'draft')]):
+        """This method will call from cron and will notify subscriber if product gets back into the stock"""
+        for record in self.search([("state", "=", "draft")]):
             if record.product_id.qty_available > 0:
                 try:
                     record._send_product_stock_notification()
-                    record.state = 'sent'
+                    record.state = "sent"
                     self.env.cr.commit()
                 except Exception as e:
                     _logger.error(e)
@@ -32,5 +32,5 @@ class ProductStockNotification(models.Model):
 
     def _send_product_stock_notification(self):
         """Send notification to registered email id"""
-        template = self.env.ref('website_stock_notify.email_template_stock_notification', False)
+        template = self.env.ref("website_stock_notify.email_template_stock_notification", False)
         template.send_mail(res_id=self.id, force_send=False)
