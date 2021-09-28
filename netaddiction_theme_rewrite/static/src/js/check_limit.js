e64b5b74043b7276db3b71cc027416d38849349c
Fabio Filipponi <fabio.filipponi@netaddiction.it>
2021-09-28 13:09:13 +0200

    commento

--- netaddiction_theme_rewrite/static/src/js/check_limit.js
+++ netaddiction_theme_rewrite/static/src/js/check_limit.js
@@ -30,41 +30,41 @@ odoo.define('netaddiction_theme_rewrite.check_order_limit', function (require) {
     });
 });
 
-
-odoo.define('netaddiction_theme_rewrite.limit_product_payment', function(require) {
-    "use strict";
-    var PaymentForm = require('payment.payment_form');
+//NON ELIMINARE é UN WORK IN PROGRESS
+// odoo.define('netaddiction_theme_rewrite.limit_product_payment', function(require) {
+//     "use strict";
+//     var PaymentForm = require('payment.payment_form');
     
-    require('web.dom_ready');
+//     require('web.dom_ready');
     
-    PaymentForm.include({
-        events: _.extend({
-            "submit": "onSubmit",
-        }),
-        onSubmit: function(ev) {
-            ev.stopPropagation();
-            ev.preventDefault();
+//     PaymentForm.include({
+//         events: _.extend({
+//             "submit": "onSubmit",
+//         }),
+//         onSubmit: function(ev) {
+//             ev.stopPropagation();
+//             ev.preventDefault();
 
-            this._rpc({
-                route: "/shop/cart/check_limit_order",
-            }).then(function (data) {
-                if(data != null){
-                    if(data['order_limit'] != null)
-                        return alert('Non Puoi ordinare più di '+data['order_limit']+' unità per questo prodotto: '+data['product_name']);
-                    else if(data['order_limit_total'] != null)
-                        return alert('Questo prodotto non è più vendibile: '+data['product_name']);
-                    else if(data.out_of_stock)
-                        return alert('Questo prodotto non è più disponibile: '+data['product_name']);
-                }
+//             this._rpc({
+//                 route: "/shop/cart/check_limit_order",
+//             }).then(function (data) {
+//                 if(data != null){
+//                     if(data['order_limit'] != null)
+//                         return alert('Non Puoi ordinare più di '+data['order_limit']+' unità per questo prodotto: '+data['product_name']);
+//                     else if(data['order_limit_total'] != null)
+//                         return alert('Questo prodotto non è più vendibile: '+data['product_name']);
+//                     else if(data.out_of_stock)
+//                         return alert('Questo prodotto non è più disponibile: '+data['product_name']);
+//                 }
                 
-                var button = $(ev.target).find('*[type="submit"]')[0]
-                if (button.id === 'o_payment_form_pay') {
-                    return this.payEvent(ev);
-                } else if (button.id === 'o_payment_form_add_pm') {
-                    return this.addPmEvent(ev);
-                }
-                return;
-            });
-        }
-    });
-});
+//                 var button = $(ev.target).find('*[type="submit"]')[0]
+//                 if (button.id === 'o_payment_form_pay') {
+//                     return this.payEvent(ev);
+//                 } else if (button.id === 'o_payment_form_add_pm') {
+//                     return this.addPmEvent(ev);
+//                 }
+//                 return;
+//             });
+//         }
+//     });
+// });
