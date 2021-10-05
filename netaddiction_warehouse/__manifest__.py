c38e60450f3ed6a21f4965c52cf3c4910a913616
Andrea Colangelo <andrea.colangelo@gmail.com>
2021-10-05 12:54:25 +0200

    [UPD][netaddiction_warehouse] Bump version

--- netaddiction_warehouse/__manifest__.py
+++ netaddiction_warehouse/__manifest__.py
@@ -4,7 +4,7 @@
     'author': "NetAddiction",
     'website': "http://www.netaddiction.it",
     'category': 'Warehouse Management',
-    'version': '14.0.1.3.2',
+    'version': '14.0.1.3.3',
     'depends': [
         'base',
         'delivery',
