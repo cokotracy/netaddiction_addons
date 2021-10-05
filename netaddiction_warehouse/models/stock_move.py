57352dcbfc11b6b6c5d056a74f08982b1083278f
Francesco Apruzzese <cescoap@gmail.com>
2021-10-05 15:21:46 +0200

    [FIX][netaddiction_warehouse] Move right quantity of a product from supplier moves interface

--- netaddiction_warehouse/models/stock_move.py
+++ netaddiction_warehouse/models/stock_move.py
@@ -51,6 +51,7 @@ class StockMove(models.Model):
             residual = int(op.product_uom_qty) - int(op.quantity_done)
             if residual >= to_remove:
                 op.quantity_done += to_remove
+                to_remove = 0
             elif residual < to_remove:
                 op.quantity_done += residual
                 to_remove -= residual
