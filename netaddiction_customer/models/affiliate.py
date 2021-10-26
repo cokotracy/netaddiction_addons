b22f8212b7ba8c1f81bf384c5f754d9924ab2fe5
Matteo <matteo.serafini@netaddiction.it>
2021-10-26 19:13:57 +0200

    Fix pr

--- netaddiction_customer/models/affiliate.py
+++ netaddiction_customer/models/affiliate.py
@@ -1,4 +1,9 @@
+import logging
+
 from odoo import models
+
+
+_logger = logging.getLogger(__name__)
 
 
 class NetaddictionAffilitateVisit(models.TransientModel):
@@ -11,6 +16,7 @@ class NetaddictionAffilitateVisit(models.TransientModel):
                 visit.action_confirm()
                 visit.write({"commission_type": "d"})
             except Exception:
+                _logger.error(f"Vista affiliato non confermata: {visit.id}")
                 continue
 
     def _create_wallet_payment(self, affiliate, amount):
@@ -30,13 +36,14 @@ class NetaddictionAffilitateVisit(models.TransientModel):
 
     def cron_affiliate_add_money_wallet(self):
         aff_visit_obj = self.env["affiliate.visit"]
-        aff_orders = aff_visit_obj.search([("state", "=", "confirm"), ("id", "=", 1138)])
+        aff_orders = aff_visit_obj.search([("state", "=", "confirm")])
         for aff_order in aff_orders:
             order = aff_order.sales_order_line_id.order_id
             if order.state == "done" and (aff_order.commission_amt and aff_order.commission_amt > 0):
                 try:
                     self._create_wallet_payment(aff_order.affiliate_partner_id, aff_order.commission_amt)
                 except Exception:
+                    _logger.error(f"Wallet non ricaricato per l'ordine {order.id}")
                     continue
                 else:
                     aff_order.write({"state": "paid"})
