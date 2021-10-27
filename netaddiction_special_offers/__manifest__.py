b42e401636340f668d156c140aba9904c795944f
Matteo <matteo.serafini@netaddiction.it>
2021-10-27 13:51:46 +0200

    fix version

--- netaddiction_special_offers/__manifest__.py
+++ netaddiction_special_offers/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Netaddiction Special Offers',
     'summary': "Offers and discount for multiplayer.com",
-    'version': '14.0.1.1.1',
+    'version': '14.0.1.1.0',
     'category': 'Sale',
     'website': 'http://www.netaddiction.it',
     'author': 'Netaddiction',
