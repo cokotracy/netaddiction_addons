864aa3f888e521391e44d8b6806154bf13236ecb
Matteo <matteo.serafini@netaddiction.it>
2021-11-03 12:51:10 +0100

    bump version

--- netaddiction_theme_rewrite/__manifest__.py
+++ netaddiction_theme_rewrite/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Netaddiction Theme Rewrite",
     "category": "eCommerce",
-    "version": "14.0.2.21.0",
+    "version": "14.0.2.23.0",
     "author": "Netaddiction",
     "license": "LGPL-3",
     "depends": [
