389abe927e9dbd01d2ea07d113b09d6322f9a0fe
Fabio Filipponi <fabio.filipponi@netaddiction.it>
2021-11-11 15:31:38 +0100

    fix paginazione su backpage

--- netaddiction_special_offers/__manifest__.py
+++ netaddiction_special_offers/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Netaddiction Special Offers',
     'summary': "Offers and discount for multiplayer.com",
-    'version': '14.0.1.6.0',
+    'version': '14.0.1.7.0',
     'category': 'Sale',
     'website': 'http://www.netaddiction.it',
     'author': 'Netaddiction',
